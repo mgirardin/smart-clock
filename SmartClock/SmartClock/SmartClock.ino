0af2b52bde595abbd9555fedce7da92467e74549
Matheus Girardin <7042521-mgirardin.inoa@users.noreply.gitlab.com>
2021-03-13 03:15:49 -0300

    small refactor

--- SmartClock/SmartClock/SmartClock.ino
+++ SmartClock/SmartClock/SmartClock.ino
@@ -4,15 +4,21 @@
 
 String wakeUpTime;
 
+/* Debug Config */
+const static int debugPortBaud = 115200;
+/* Debug Config */
+
 /* Server Config Variables */
-const byte DNS_PORT = 53;
+const static byte DNS_PORT = 53;
 DNSServer dnsServer;
 ESP8266WebServer webServer(80);
+String softApSsid = "SmartClock";
+String softApPassword = "12341234";
 IPAddress apIP(192, 168, 4, 1);
 /* Server Config Variables */
 
 /* HTML Pages */
-String captivePortalHTML = ""
+const static String captivePortalHTML = ""
                       "<!DOCTYPE html><html lang='en'><head>"
                       "<meta name='viewport' content='width=device-width'>"
                       "<title>SmartClock</title></head><body>"
@@ -32,16 +38,22 @@ void handle_wake_up_time() {
   Serial.print("Number of args: ");
   Serial.println(webServer.args());
   if(webServer.args() >= 1){
-    Serial.print("Setting wake up time to arg 1:");
+    Serial.print("Setting wake up time to arg: ");
     Serial.println(webServer.arg(0));
     wakeUpTime = webServer.arg(0);
   } else {
     Serial.println("Not changing wake up time because request didnt have any argument");
   }
+  
+  if(webServer.hasHeader("Date")){
+    Serial.print("Date header: ");
+    Serial.println(webServer.header("Date"));
+  }
   Serial.print("Updated wake up time: ");
   Serial.println(wakeUpTime);
   webServer.send(200, "text/html", "ok");
 }
+
 void handle_not_found() {
     Serial.println("Accessing captive portal");
     webServer.send(200, "text/html", captivePortalHTML);
@@ -50,19 +62,22 @@ void handle_not_found() {
 
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(debugPortBaud);
   Serial.println();
 
   Serial.println("Setting soft-AP ... ");
   WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
-  boolean softApResult = WiFi.softAP("SmartClock", "12341234");
-  if(softApResult) Serial.println("Ready");
-  else Serial.println("Failed!");
+  boolean softApGenerationResult = WiFi.softAP(softApSsid, softApPassword);
+  if(softApGenerationResult) Serial.println("Soft-AP ready");
+  else Serial.println("Soft-AP failed!");
   
   dnsServer.start(DNS_PORT, "*", apIP);
 
+  /* Endpoint registers */
   webServer.on("/wake_up_time", HTTP_POST, handle_wake_up_time);
   webServer.onNotFound(handle_not_found);
+  /* Endpoint registers */
+
   webServer.begin();
 }
 
